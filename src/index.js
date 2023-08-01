f9937248a14d39a937d8b14b2450d8f8482616ee
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-08-01 14:47:52 +0200

    Install css loaders and add css file to src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("God is amazing");
+import "./style.css";
+
+const body = document.querySelector("body");
+body.innerHTML = "<header><h1>Billy's Burgers</h1><nav><button>Home</button><button>Menu</button><button>Contact</button></nav></header><main></main>"
