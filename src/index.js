692ba1c8bdc8efde78b8b9a4a42a7a66a27a2a0a
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-08-03 13:13:49 +0200

    Update index.js to import functions from the modules to use for the display

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import "./style.css";
+import home from "./home-module/home.js";
+import menu from "./menu-module/menu.js";
+import contact from "./contact-module/contact.js";
 
 const body = document.querySelector("body");
-body.innerHTML = "<header><h1>Billy's Burgers</h1><nav><button>Home</button><button>Menu</button><button>Contact</button></nav></header><main></main>"
+body.innerHTML = "<header><h1>Billy's Burgers</h1><nav><button class='home'>Home</button><button class='menu'>Menu</button><button class='contacts'>Contact</button></nav></header><main></main><footer>Made by <a href='https://github.com/Ngonidzashe-Zvenyika'>Ngonidzashe Zvenyika</a></footer>";
+
+const main = document.querySelector("main");
+main.appendChild(home());
+
+const buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+    if (button.classList.contains("home")) button.classList.add("selected");
+    button.addEventListener("click", () => {
+        main.replaceChildren();
+        buttons.forEach(button => button.classList.remove("selected"));
+        button.classList.add("selected"); 
+        switch (true) {
+            case (button.classList.contains("home")):  
+                main.appendChild(home());
+                break;
+            case (button.classList.contains("menu")):   
+                main.appendChild(menu());
+                break;
+            case (button.classList.contains("contacts")):   
+                main.appendChild(contact());
+                break;
+        };
+    });
+};
+
