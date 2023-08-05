c20a5a29a013c18bba44d3eddf7cab58277705a9
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-08-05 16:06:26 +0200

    Update webpack configuration to process images as an asset

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    entry: {
+        index: "./src/index.js",
+    },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
         ],
     },
 };
