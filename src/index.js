cce75a36c91398a71473acb48f0de10c1862543f
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-08-05 16:08:27 +0200

    Add background image and update index.js to make it visible

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import "./style.css";
 import home from "./home-module/home.js";
 import menu from "./menu-module/menu.js";
 import contact from "./contact-module/contact.js";
+import background from "./background.jpg";
 
 const body = document.querySelector("body");
 body.innerHTML = "<header><h1>Billy's Burgers</h1><nav><button class='home'>Home</button><button class='menu'>Menu</button><button class='contacts'>Contact</button></nav></header><main></main><footer>Made by <a href='https://github.com/Ngonidzashe-Zvenyika'>Ngonidzashe Zvenyika</a></footer>";
+
+const backdrop = document.createElement("img");
+backdrop.classList.add("backdrop");
+backdrop.src = background;
+backdrop.alt = "background";
+body.appendChild(backdrop);
 
 const main = document.querySelector("main");
 main.appendChild(home());
@@ -29,4 +36,3 @@ for (const button of buttons) {
         };
     });
 };
-
